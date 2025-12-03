1c740dc7641edeb8bffe0a237f7ceed70fee60ea
yashashree-naik-01 <yashashree.naik.01@gmail.com>
2025-12-03 23:42:53 +0530

    Add files via upload

--- Server/src/validators/leaveValidators.js
+++ Server/src/validators/leaveValidators.js
@@ -1,1 +1,16 @@
+const Joi = require('joi');
+
+const leaveCreateSchema = Joi.object({
+  employeeId: Joi.string().max(50).required(),
+  leaveType: Joi.string().valid('Sick', 'Casual', 'Paid', 'Emergency').required(),
+  startDate: Joi.date().iso().required(),
+  endDate: Joi.date().iso().required(),
+  reason: Joi.string().max(500).allow(''),
+  days: Joi.number().min(0.5).required(),
+  emergency: Joi.boolean().default(false)
+});
+
+module.exports = {
+  leaveCreateSchema
+};
 
